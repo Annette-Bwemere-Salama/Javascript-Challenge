be783418d22bbfdcec582c25efe62d7ef6a1d757
Annette Bwemere <bwemereannette@gmail.com>
2023-05-22 22:23:12 +0200

    remlace bay antsrique

---
+++ map/FahrenheitToCelicius/positionMap/solution.js
@@ -0,0 +1,9 @@
+/**
+ *  Remplacer les positions impaires du tablaeu farhénat par des « * ». Pour cela nous avons
+besoin de la position de l’élément (index).
+ * 
+ */
+
+
+// [45, 32, 0, 50, 75, 80, 120, 99].map((v, m) => (m % 2 == 0) ? v : '*')
+console.log([45, 32, 0, 50, 75, 80, 120, 99].map((v, m) => (m % 2 == 0) ? v : '*'))
