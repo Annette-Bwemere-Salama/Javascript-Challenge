5a438a2fcef0e8bf67123d82740052261dc55e8c
Annette Bwemere <bwemereannette@gmail.com>
2023-05-25 11:15:10 +0200

    filter methode

---
+++ filter/README.md
@@ -0,0 +1,3 @@
+▪
+FILTER
+**Exemples** : récupérer les nombres pairs du tableau Fahrenheit puis récupérer les éléments qui sont aux positions paires.
---
+++ filter/solution.js
@@ -0,0 +1,9 @@
+/**
+ * ligne 6 initialisation du tableau et affectation des élements pour la suite il ya la méthode filter 
+ * 
+ */
+
+let fahreinheit = [1, 12, 24, 56, 55, 30, 50].filter((n) => n % 2 == 0);
+console.log('====================================');
+console.log(fahreinheit);
+console.log('====================================');
